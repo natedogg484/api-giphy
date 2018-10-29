e52456575ef4f7f270339b12d02fb949ea9a7188
natedogg484 <nathangschmidt@gmail.com>
2018-10-29 19:42:13 -0400

    ohjeez

--- gifs.js
+++ gifs.js
@@ -5,44 +5,76 @@ var gifs = ["cat", "hamster"];
 console.log(gifs)
 
 
+function displayMovieInfo() {
+    // query set up
+    var gif = $(this).attr("data-set");
 
-// query set up
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=pEw1X1OTCoK8MgTCigxgxlwa9UrEx3NP&q=cheesy puffs&limit=10&offset=0&rating=PG&lang=en";
+    // Constructing a queryURL using the gif name
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        gif + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    var results = response.data
-    console.log(results)
-});
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var results = response.data
+        for (var i = 0; i < results.length; i++) {
+
+            // Creating and storing a div tag
+            var gifDiv = $("<div>");
+
+            // Creating a paragraph tag with the result item's rating
+            var p = $("<p>").text("Rating: " + results[i].rating);
+
+            // Creating and storing an image tag
+            var gifImage = $("<img>");
+            // Setting the src attribute of the image to a property pulled off the result item
+            gifImage.attr("src", results[i].images.fixed_height.url);
+
+            // Appending the paragraph and image tag to the gifDiv
+            gifDiv.append(p);
+            gifDiv.append(gifImage);
+
+            // Prependng the gifDiv to the HTML page in the "#gifs-appear-here" div
+            $("#gifs-appear-here").prepend(gifDiv);
+        }
+    });
+}
 
 function makeButtons() {
 
     // Deleting the movies prior to adding new movies
     // (this is necessary otherwise you will have repeat buttons)
-    $("#buttons-view").empty();
+    $("#gif-buttons").empty();
 
     // Looping through the array of movies
     for (var i = 0; i < gifs.length; i++) {
 
         // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        var a = $("<button>");
-        // Adding a class of movie-btn to our button
-        a.addClass("gif-btn");
-        // Adding a data-attribute
-        a.attr("data-name", gifs[i]);
-        // Providing the initial button text
-        a.text(gifs[i]);
-        // Adding the button to the buttons-view div
-        $("#buttons-view").append(a);
+        // Creating and storing a div tag
+        var gifDiv = $("<div>");
+
+        // Creating a paragraph tag with the result item's rating
+        var p = $("<p>").text("Rating: " + results[i].rating);
+
+        // Creating and storing an image tag
+        var gifImage = $("<img>");
+        // Setting the src attribute of the image to a property pulled off the result item
+        gifImage.attr("src", results[i].images.fixed_height.url);
+
+        // Appending the paragraph and image tag to the gifDiv
+        gifDiv.append(p);
+        gifDiv.append(gifImage);
+
+        // Prependng the gifDiv to the HTML page in the "#gifs-appear-here" div
+        $("#gifs-appear-here").prepend(gifDiv);
     }
 }
 
+
 // This function handles events where a movie button is clicked
-$("#add-gif").on("click", function (event) {
+$("#gif-create").on("click", function (event) {
     event.preventDefault();
     // This line grabs the input from the textbox
     var gif = $("#gif-input").val().trim();
--- hardpush.js
+++ hardpush.js
@@ -45,3 +45,26 @@
         }
       });
   });
+
+
+// $("#gif-create").on("click", function(){
+//     // Deleting the movies prior to adding new movies
+//     // (this is necessary otherwise you will have repeat buttons)
+//     $("#buttons-view").empty();
+
+//     // Looping through the array of movies
+//     for (var i = 0; i < gifs.length; i++) {
+
+//       // Then dynamicaly generating buttons for each movie in the array
+//       // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+//       var a = $("<button>");
+//       // Adding a class of movie-btn to our button
+//       a.addClass("gif-btn");
+//       // Adding a data-attribute
+//       a.attr("data-set", gifs[i]);
+//       // Providing the initial button text
+//       a.text(gifs[i]);
+//       // Adding the button to the buttons-view div
+//       $("#gif-buttons").append(a);
+//     }
+//   });
