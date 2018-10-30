85e1052e5c2b56370b060d7269df844a122775fc
natedogg484 <nathangschmidt@gmail.com>
2018-10-29 20:32:07 -0400

    woogit add .!

--- gifs.js
+++ gifs.js
@@ -4,11 +4,15 @@
 var gifs = ["cat", "hamster"];
 console.log(gifs)
 
-
-function displayMovieInfo() {
-    // query set up
+$(document).on("click", ".gif-btn", function(event){
+    event.preventDefault();
     var gif = $(this).attr("data-set");
+    displayGifInfo(gif)
+});
 
+function displayGifInfo(gif) {
+    // query set up
+    console.log(gif)
     // Constructing a queryURL using the gif name
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         gif + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -17,7 +21,7 @@ function displayMovieInfo() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
+        console.log(response)
         var results = response.data
         for (var i = 0; i < results.length; i++) {
 
@@ -43,35 +47,26 @@ function displayMovieInfo() {
 }
 
 function makeButtons() {
-
     // Deleting the movies prior to adding new movies
     // (this is necessary otherwise you will have repeat buttons)
     $("#gif-buttons").empty();
 
     // Looping through the array of movies
     for (var i = 0; i < gifs.length; i++) {
-
         // Then dynamicaly generating buttons for each movie in the array
-        // Creating and storing a div tag
-        var gifDiv = $("<div>");
-
-        // Creating a paragraph tag with the result item's rating
-        var p = $("<p>").text("Rating: " + results[i].rating);
-
-        // Creating and storing an image tag
-        var gifImage = $("<img>");
-        // Setting the src attribute of the image to a property pulled off the result item
-        gifImage.attr("src", results[i].images.fixed_height.url);
-
-        // Appending the paragraph and image tag to the gifDiv
-        gifDiv.append(p);
-        gifDiv.append(gifImage);
-
-        // Prependng the gifDiv to the HTML page in the "#gifs-appear-here" div
-        $("#gifs-appear-here").prepend(gifDiv);
-    }
-}
-
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adding a class of movie-btn to our button
+        a.addClass("gif-btn");
+        // Adding a data-attribute
+        a.attr("data-set", gifs[i]);
+        // Providing the initial button text
+        a.text(gifs[i]);
+        // Adding the button to the buttons-view div
+        $("#gif-buttons").append(a);
+      }
+} 
+makeButtons();
 
 // This function handles events where a movie button is clicked
 $("#gif-create").on("click", function (event) {
